d242e6e7b7cdb74f7add8109bbf3914c68a61a23
Prathamesh79097 <prathameshspatil1305@gmail.com>
2026-04-20 21:39:43 +0530

    update changes

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,10 +1,12 @@
 class Question {
+  final int? id;
   final String question;
   final List<String> options;
   final String answer;
   final String? image;
 
   Question({
+    this.id,
     required this.question,
     required this.options,
     required this.answer,
@@ -13,6 +15,7 @@ class Question {
 
   factory Question.fromJson(Map<String, dynamic> json) {
     return Question(
+      id: json['id'],
       question: json['question'] ?? 'Unknown',
       options: List<String>.from(json['options'] ?? []),
       answer: json['answer'] ?? '',
--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/quiz_provider.dart';
-import '../providers/auth_provider.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
